c881b3690a4a564461d4284c5522904a429b1c26
Emilio Cunha <36512099+cunhaemilio@users.noreply.github.com>
2018-08-22 14:44:37 -0300

    Delete learn-more.rb

--- features/step_definitions/learn-more.rb
+++
@@ -1,8 +0,0 @@
-Então("Navego pelos links laterais") do
-    find('body > div.ng-scope.global.choko.anonymous.documentation > div.row-content.row-fluid > div > div.column-sidebar.col-md-3 > div > div > div.panel-wrapper.ng-scope > ul > li:nth-child(2) > a').click
-    find('body > div.ng-scope.global.choko.anonymous.documentation > div.row-content.row-fluid > div > div.column-sidebar.col-md-3 > div > div > div.panel-wrapper.ng-scope > ul > li:nth-child(3) > a').click
-    find('body > div.ng-scope.global.choko.anonymous.documentation > div.row-content.row-fluid > div > div.column-sidebar.col-md-3 > div > div > div.panel-wrapper.ng-scope > ul > li:nth-child(4) > a').click
-    find('body > div.ng-scope.global.choko.anonymous.documentation > div.row-content.row-fluid > div > div.column-sidebar.col-md-3 > div > div > div.panel-wrapper.ng-scope > ul > li:nth-child(5) > a').click
-    find('body > div.ng-scope.global.choko.anonymous.documentation > div.row-content.row-fluid > div > div.column-sidebar.col-md-3 > div > div > div.panel-wrapper.ng-scope > ul > li:nth-child(6) > a').click
-    find(:xpath, "/html/body/div[1]/div[4]/div/div[1]/div/div/div[2]/ul/li[7]/a").click
-end
