84cc8d5f53f9610978e4e66de54b18576843661a
Emilio Cunha <36512099+cunhaemilio@users.noreply.github.com>
2018-09-19 17:51:25 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,6 @@
 require 'selenium-webdriver'
 require 'capybara'
 require 'capybara/cucumber'
-#require 'capybara/poltergeist'
-#require 'site_prism'
-
-
 
 $env = ENV['BROWSER']
 $headless = ENV['HEADLESS']
